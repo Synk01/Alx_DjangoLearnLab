e892d17ab1c2eaa458fd2cf122073f1ee36e02de
Synk01 <90985840+Synk01@users.noreply.github.com>
2025-10-05 23:39:54 +0100

    Delete django_blog directory

--- django_blog/blog/tests.py
+++
@@ -1,6 +0,0 @@
-from django.urls import reverse, resolve
-
-def test_urls():
-    assert reverse('blog:comment-create', kwargs={'post_id': 1}) == '/posts/1/comments/new/'
-    assert reverse('blog:comment-update', kwargs={'pk': 1}) == '/comment/1/update/'
-    assert reverse('blog:comment-delete', kwargs={'pk': 1}) == '/comment/1/delete/'
