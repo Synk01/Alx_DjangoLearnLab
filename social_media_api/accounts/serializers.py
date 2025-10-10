be9d2ab71d860bb45e319723aa641ddaa5e6936a
Synk01 <90985840+Synk01@users.noreply.github.com>
2025-10-10 13:27:54 +0100

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,15 +1,30 @@
+
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
-from .models import User
 from accounts.models import CustomUser
-from .models import CustomUser
 
-#CustomUser = get_user_model()
 User = get_user_model()
-#.objects.create_user
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+
+    def create(self, validated_data):
+        # create user
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        # ✅ create token
+        Token.objects.create(user=user)
+        return user
+
 
 class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,6 +36,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'first_name', 'last_name']
+
 
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
@@ -37,7 +53,6 @@ class UserLoginSerializer(serializers.Serializer):
             data['user'] = user
         else:
             raise serializers.ValidationError("Must include 'username' and 'password'.")
-
         return data
 
 
@@ -57,66 +72,8 @@ class CustomUserSerializer(serializers.ModelSerializer):
         )
         return user
 
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'password', 'email')  # Include necessary fields
 
-    def create(self, validated_data):
-        user = User(**validated_data)
-        user.set_password(validated_data['password'])  # Hash the password
-        
-        
-        user.save()
-        return user
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    password = serializers.CharField(required=True, write_only=True)
-
-    def validate(self, data):
-        serializer = LoginSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email']
-    
 class UserFollowSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username')    
-
-class UserRegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User(**validated_data)
-        user.set_password(validated_data['password'])  # Hash the password
-        user.save()
-        return user
-    
-class RegistrationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
-
-    def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        return user
-
-    print("Debug:", CustomUser._meta)
-    print("Model in Serializer:", User)
-    print("CustomUser model:", CustomUser)  # Add this in serializers.py
-
-     
+        fields = ('id', 'username')
